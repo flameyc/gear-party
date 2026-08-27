e18996bf870e0486545ea2c1798098b768e16acd
flameyc <g383047884@gmail.com>
2026-08-27 16:51:24 +0800

    deploy: game.js

---
+++ game.js
@@ -0,0 +1,17 @@
+// 齿轮派对 —— 微信小游戏入口
+require('./js/config.js');
+require('./js/platform.js');
+require('./js/audio.js');
+require('./js/render.js');
+require('./js/players.js');
+require('./js/difficulty.js');
+require('./js/minigames/common.js');
+require('./js/minigames/minefield.js');
+require('./js/minigames/gearstorm.js');
+require('./js/minigames/ladder.js');
+require('./js/minigames/patternpad.js');
+require('./js/minigames/stealparts.js');
+require('./js/minigames/assembly.js');
+require('./js/flow.js');
+
+GP.flow.boot();
